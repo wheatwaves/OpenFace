4319e0790b59c4806575fabc48a448780d27456b
wangsen <wheatwang@sohu-inc.com>
2016-11-02 09:34:51 -0400

    add split_data.py

---
+++ exe/FeatureExtraction/split_data.py
@@ -0,0 +1,14 @@
+import os
+list_dirs = os.walk('/usr0/home/senw1/OpenFace/annotation_txt')
+looking_down_intervals = {}
+for root, dirs, files in list_dirs:
+	for file in files:
+		name = file.split('.')[0]
+		if len(name):
+			looking_down_intervals[name] = []
+			with open(root + file) as f:
+				for line in f.readlines():
+					line = line.strip().split('\t')
+					s, e = float(line[2]), float(line[4])
+					looking_down_intervals[name].append((s, e))
+print looking_down_intervals
