018d83f7de79e3a9b026660538f2b8b0be10f580
wangsen <wheatwang@sohu-inc.com>
2016-11-02 09:50:31 -0400

    add split_data.py

--- exe/FeatureExtraction/split_data.py
+++ exe/FeatureExtraction/split_data.py
@@ -1,7 +1,7 @@
 import os
-list_dirs = os.walk('/usr0/home/senw1/OpenFace/annotation_txt/')
+annotation_dirs = os.walk('/usr0/home/senw1/OpenFace/annotation_txt/')
 looking_down_intervals = {}
-for root, dirs, files in list_dirs:
+for root, dirs, files in annotation_dirs:
 	for file in files:
 		name = file.split('.')[0]
 		if len(name):
@@ -11,4 +11,25 @@ for root, dirs, files in list_dirs:
 					line = line.strip().split('\t')
 					s, e = float(line[3]), float(line[5])
 					looking_down_intervals[name].append((s, e))
-print looking_down_intervals
+feature_dirs = os.walk('/usr0/home/senw1/OpenFace/output_features/')
+for root, dirs, files in feature_dirs:
+	for file in files:
+		name = file.split('.')[0]
+		if len(name):
+			if name in looking_down_intervals.keys():
+				with open(root + file) as f:
+					for line in f.readlines()[1:]:
+						timestamp = float(line.split(',')[1].strip())
+						flag = False
+						for interval in looking_down_intervals[name]:
+							if timestamp >= interval[0] and timestamp <= interval[1]:
+								flag = True
+								break
+						if flag:
+							with open('/usr0/home/senw1/OpenFace/training_data/positive.txt', 'a') as g:
+								g.write(line)
+						else:
+							with open('/usr0/home/senw1/OpenFace/training_data/negative.txt', 'a') as g:
+								g.write(line)
+
+
